4a90d6415f4ad41ba7bea50e391dca0a6f3e0c7e
JuanL <jslopezg@gmail.com>
2023-08-25 23:45:52 -0500

    Pydantic validation on POST Body

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sqlalchemy
 
 from fastapi import FastAPI, Request
 
+from pydantic import BaseModel, ValidationError
+
 DATABASE_URL = 'postgresql://postgres@localhost:5432/store'
 
 database = databases.Database(DATABASE_URL)
@@ -33,6 +35,15 @@ readers = sqlalchemy.Table(
 # engine = sqlalchemy.create_engine(DATABASE_URL)
 # metadata.create_all(engine)
 
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    reader_id: int
+
+class ReaderCreate(BaseModel):
+    first_name: str
+    last_name: str
+
 app = FastAPI()
 
 @app.on_event('startup')
@@ -48,16 +59,47 @@ async def get_all_books():
     query = books.select()
     return await database.fetch_all(query)
 
+
+# @app.post('/books/')
+# async def create_book(request: Request):
+#     data = await request.json()
+#     query = books.insert().values(**data)
+#     last_record_id = await database.execute(query)
+#     return {'id': last_record_id}
+
 @app.post('/books/')
-async def create_book(request: Request):
-    data = await request.json()
-    query = books.insert().values(**data)
+async def create_book(data: BookCreate):
+    try:
+        book_data = BookCreate(**data.dict())
+    except ValidationError as e:
+        return {'error': str(e)}
+
+    query = books.insert().values(**book_data.dict())
     last_record_id = await database.execute(query)
+
     return {'id': last_record_id}
 
+@app.get('/readers/')
+async def get_all_readers():
+    query = readers.select()
+    return await database.fetch_all(query)
+
+
+# @app.post('/readers/')
+# async def create_reader(request: Request):
+#     data = await request.json()
+#     query = readers.insert().values(**data)
+#     last_record_id = await database.execute(query)
+#     return {'id': last_record_id}
+
 @app.post('/readers/')
-async def create_reader(request: Request):
-    data = await request.json()
-    query = readers.insert().values(**data)
+async def create_reader(request: ReaderCreate):
+    try:
+        reader_data = ReaderCreate(**request.dict())
+    except ValidationError as e:
+        return {'error': str(e)}
+    
+    query = readers.insert().values(**reader_data.dict())
     last_record_id = await database.execute(query)
+    
     return {'id': last_record_id}
